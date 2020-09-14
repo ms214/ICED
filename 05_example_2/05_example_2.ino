b09a8c5cb530e5ad28fb91ccfebe4dc5277159bc
ms214 <ms214@ms214.kr>
2020-09-14 10:40:20 +0900

    add 05_example_02

---
+++ 05_example_2/05_example_2.ino
@@ -0,0 +1,38 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  //digitalWrite(PIN_LED, 1); // turn off LED.
+}
+
+void loop() {
+  count = count +1 ;
+  Serial.println(count);
+  if(count <= 10 && count > 1){
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle);
+    delay(100);
+  }else if(count > 10){
+    digitalWrite(PIN_LED, 1);
+    delay(1000);
+  }else{
+    digitalWrite(PIN_LED, 0);
+    delay(1000);
+  }
+}
+
+int toggle_state(int toggle) {
+  if(toggle == 1){
+    toggle = 0;
+  }else{
+    toggle = 1;
+  }
+  return toggle;
+}
